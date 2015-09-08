69248a79c3a81bca61b1c67c4643fb5fdb291915
unknown <beethoven99@126.com>
2015-09-08 13:35:24 +0800

    make the demo code more precise

--- src/main/java/org/yage/html/demo/Main.java
+++ src/main/java/org/yage/html/demo/Main.java
@@ -1,10 +1,8 @@
 package org.yage.html.demo;
 
-import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
-
+import java.io.OutputStreamWriter;
+import java.net.URL;
 import org.yage.html.ContentExtractor;
 import org.yage.html.ExtractUtils;
 
@@ -19,23 +17,14 @@ public class Main {
     	ContentExtractor me=new ContentExtractor();
         me.setUseTitleTag(true);
         
-        String fname="a1.htm";
-        InputStream istream=null;
-        FileWriter fwriter=null;
         try{
-        	istream=ClassLoader.getSystemResourceAsStream(fname);
-    		fwriter=new FileWriter(new File("out.txt"));
-        	me.doExtracting(ExtractUtils.readContentFromStream(istream), fwriter);
+        	URL url=new URL("http://tech.sina.com.cn/t/4g/2014-05-10/05249370488.shtml");
+        	url=new URL("http://tech.sina.com.cn/mobile/n/2013-08-23/06328667223.shtml");
+        	//me.doExtracting(ExtractUtils.readContentFromStream(url.openStream(), "gb2312"), Files.newBufferedWriter(Paths.get("dd.txt"), Charset.forName("gbk")));
+        	me.doExtracting(ExtractUtils.readContentFromStream(url.openStream(), "gb2312"),new OutputStreamWriter(System.out));
         } catch (IOException e) {
 			e.printStackTrace();
-		} finally{
-			try {
-				fwriter.close();
-				istream.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
 		}
-        System.out.println("run finished.");
+        
     }
 }
